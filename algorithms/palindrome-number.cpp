fd51ee262f39c4c44bd21951c4b5bb21cd51cd26
zziz <fvzaur@gmail.com>
2018-02-16 20:20:20 +0900

    Palindrome Number

---
+++ algorithms/palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)   return false;       // negative number cannot be palindrome
+        int rev = 0, y = x;
+        while (y) {
+            int temp = rev * 10 + y % 10;
+            if (temp / 10 != rev)
+                return false;           // prevent overflow
+            rev = temp;
+            y /= 10;
+        }
+        if(rev == x)    return true;
+        else return false;
+        
+    }
+};
