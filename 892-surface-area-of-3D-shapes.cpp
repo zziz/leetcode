a9fe99888ce88ad10d36c72080101d2bd8f43ea7
zziz <fvzaur@gmail.com>
2018-08-29 23:16:25 +0900

    some change to this file

--- 892-surface-area-of-3D-shapes.cpp
+++ 892-surface-area-of-3D-shapes.cpp
@@ -8,13 +8,10 @@ public:
                 
         for(int i = 0; i < width; i++){
             for(int j = 0; j < height; j++){
-                // cout << grid[i][j] << endl;
-                // if(i == 0)
-                //up
                 int dd = 0;
-                if(i > 0)           dd -= min(grid[i][j], grid[i - 1][j]);   // up
-                if(j > 0)           dd -= min(grid[i][j], grid[i][j - 1]);   // left
-                if(i < width - 1)   dd -= min(grid[i][j], grid[i + 1][j]);    //right
+                if(i > 0)           dd -= min(grid[i][j], grid[i - 1][j]);  
+                if(j > 0)           dd -= min(grid[i][j], grid[i][j - 1]);  
+                if(i < width - 1)   dd -= min(grid[i][j], grid[i + 1][j]);  
                 if(j < height - 1)  dd -= min(grid[i][j], grid[i][j + 1]);
                 
                 if(grid[i][j] > 0)  total += 2 + 4 * grid[i][j] + dd;                
